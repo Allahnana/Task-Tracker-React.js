c0aaadca17249dcebfde474446da331cbf689d98
Allahnana <usmanallahnana@gmail.com>
2022-10-27 23:21:02 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import PropTypes from 'prop-types';
+import Button from './Button';
+
+const Header = ({title}) => {
+  const onClick = () => {
+    console.log('dev_abdul')
+  }
+  return (
+
+    <div >
+        <header  className='header' >
+            <h1>{title}</h1>
+            <Button onClick={onClick} color = 'green' text='Add'/>
+           
+        </header>
+    </div>
+  )
+
+  
+}
+
+Header.defaultProps = {
+    title : 'Task Tracker'
+
+  }
+
+  Header.propTypes = {
+    title: PropTypes.string.isRequired,
+  }
+
+export default Header
